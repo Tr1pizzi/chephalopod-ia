e6bac1e978fdb81568dad646798f3f073cf3c8b2
Tr1pizzi <andregimbri@gmail.com>
2025-04-27 17:59:53 +0200

    euristica prima implementazione

--- prof/playingStrategies.py
+++ prof/playingStrategies.py
@@ -155,8 +155,99 @@ def h_alphabeta_search(game, state, cutoff=cutoff_depth(2)):
 
     return max_value(state, -infinity, +infinity, 0)
 
-def h (board, player):
-    """implementare euristica qui"""
-    
-    return 0
+import math
 
+# Funzioni di utilità per il Cephalopod
+def get_adjacent_cells(r, c, size=5):
+    adj = []
+    for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        nr, nc = r + dr, c + dc
+        if 0 <= nr < size and 0 <= nc < size:
+            adj.append((nr, nc))
+    return adj
+
+def all_empty_cells(board, size=5):
+    empties = []
+    for r in range(size):
+        for c in range(size):
+            if board[r][c] is None:
+                empties.append((r, c))
+    return empties
+
+# Euristica avanzata per Cephalopod
+def evaluate_state(board, player):
+    """
+    board: matrice size x size con None o dict {'owner': "Blue"/"Red", 'pip': int}
+    player: "Blue" o "Red"
+    """
+    opponent = "Red" if player == "Blue" else "Blue"
+    score = 0
+    size = len(board)
+
+    # 1) Differenza pezzi e somma pip
+    my_pieces = my_pips = 0
+    op_pieces = op_pips = 0
+    for r in range(size):
+        for c in range(size):
+            cell = board[r][c]
+            if cell:
+                if cell['owner'] == player:
+                    my_pieces += 1
+                    my_pips   += cell['pip']
+                else:
+                    op_pieces += 1
+                    op_pips   += cell['pip']
+    score += 10 * (my_pieces - op_pieces)
+    score +=  1 * (my_pips   - op_pips)
+
+    # 2) Controllo centro
+    center_bonus = 0
+    centers = [(i, j) for i in range(1, size-1) for j in range(1, size-1)]
+    for r, c in centers:
+        cell = board[r][c]
+        if cell:
+            center_bonus +=  2 if cell['owner'] == player else -2
+    score += center_bonus
+
+    # 3) Potenziale di cattura
+    def can_capture(r, c, plyr):
+        adjacent = get_adjacent_cells(r, c, size)
+        for (r2, c2) in adjacent:
+            for (r3, c3) in adjacent:
+                if (r2, c2) < (r3, c3):
+                    a = board[r2][c2]
+                    b = board[r3][c3]
+                    if a and b and a['owner'] != plyr and b['owner'] != plyr:
+                        if a['pip'] + b['pip'] <= 6:
+                            return True
+        return False
+
+    my_caps = sum(1 for (r, c) in all_empty_cells(board, size)
+                  if can_capture(r, c, player))
+    op_caps = sum(1 for (r, c) in all_empty_cells(board, size)
+                  if can_capture(r, c, opponent))
+    score += 5 * (my_caps - op_caps)
+
+    return score
+
+# Esempio di integrazione in playingStrategies:
+def h(board_state, player):
+    """
+    board_state: istanza di Board con attributi .board e .size
+    player: "Blue" o "Red"
+    """
+    # Estrai la matrice grezza dal Board
+    size = board_state.size
+    raw_board = board_state.board  # matrice size×size di None o (owner, pip)
+
+    # Converti in formato atteso da evaluate_state
+    board = [[None] * size for _ in range(size)]
+    for r in range(size):
+        for c in range(size):
+            cell = raw_board[r][c]
+            if cell is not None:
+                owner, pip = cell
+                board[r][c] = {'owner': owner, 'pip': pip}
+
+    # Valuta lo stato convertito
+    return evaluate_state(board, player)
