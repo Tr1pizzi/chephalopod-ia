556d685004f371552315ea063a8f89089e06eb72
Tr1pizzi <andregimbri@gmail.com>
2025-05-05 17:01:21 +0200

    sdfg

--- prof/playerExampleAlpha.py
+++ prof/playerExampleAlpha.py
@@ -1,4 +1,5 @@
 import math
+<<<<<<< HEAD
 import multiprocessing
 from multiprocessing import Manager
 # The moves of player have the form (x,y), where y is the column number and x the row number (starting with 0)
@@ -9,6 +10,17 @@ def playerStrategy(game, state):
     manager = Manager()
     shared_cache = manager.dict()
     _, move = h_alphabeta_search(game, state, cutoff, shared_cache)
+=======
+
+# The moves of player have the form (x,y), where y is the column number and x the row number (starting with 0)
+infinity = math.inf
+
+def playerStrategy (game,state):
+    cutOff = 3 # The depth of the search tree. It can be changed to test the performance of the player.
+    # The player uses the alphabeta search algorithm to find the best move.
+    value,move = h_alphabeta_search(game,state,cutoff_depth(cutOff))
+  
+>>>>>>> 58757c0 (Co-authored-by: Alexxhhh <Alexxhhh@users.noreply.github.com>)
     return move
 
 def cache1(function):
@@ -19,6 +31,7 @@ def cache1(function):
             cache[x] = function(x, *args)
         return cache[x]
     return wrapped
+<<<<<<< HEAD
 def max_value(game, state, alpha, beta, depth, cutoff, fi, cache):
     player = state.to_move
     key = (state, depth, "max")
@@ -41,7 +54,7 @@ def max_value(game, state, alpha, beta, depth, cutoff, fi, cache):
         cache[key] = result
         fi.send(result)
         fi.close()
-        return
+        returnasdasdasd
 
     best_val, best_move = -infinity, None
     children = []
@@ -156,6 +169,60 @@ def get_adjacent_cells(r, c):
         adj.append((r, c - 1))
     if c < 4:  # destra
         adj.append((r, c + 1))
+=======
+
+def cutoff_depth(d):
+    """A cutoff function that searches to depth d."""
+    return lambda game, state, depth: depth > d
+
+def h_alphabeta_search(game, state, cutoff=cutoff_depth(2)):
+    """Search game to determine best action; use alpha-beta pruning.
+    As in [Figure 5.7], this version searches all the way to the leaves."""
+
+    player = state.to_move
+
+    @cache1
+    def max_value(state, alpha, beta, depth):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        if cutoff(game, state, depth):
+            return h(state, player), None
+        v, move = -infinity, None
+        for a in game.actions(state):
+            v2, _ = min_value(game.result(state, a), alpha, beta, depth+1)
+            if v2 > v:
+                v, move = v2, a
+                alpha = max(alpha, v)
+            if v >= beta:
+                return v, move
+        return v, move
+
+    @cache1
+    def min_value(state, alpha, beta, depth):
+        if game.is_terminal(state):
+            return game.utility(state, player), None
+        if cutoff(game, state, depth):
+            return h(state, player), None
+        v, move = +infinity, None
+        for a in game.actions(state):
+            v2, _ = max_value(game.result(state, a), alpha, beta, depth + 1)
+            if v2 < v:
+                v, move = v2, a
+                beta = min(beta, v)
+            if v <= alpha:
+                return v, move
+        return v, move
+
+    return max_value(state, -infinity, +infinity, 0)
+
+# Funzioni di utilità per il Cephalopod
+def get_adjacent_cells(r, c, size=5):
+    adj = []
+    for dr, dc in [(-1, 0), (1, 0), (0, -1), (0, 1)]:
+        nr, nc = r + dr, c + dc
+        if 0 <= nr < size and 0 <= nc < size:
+            adj.append((nr, nc))
+>>>>>>> 58757c0 (Co-authored-by: Alexxhhh <Alexxhhh@users.noreply.github.com>)
     return adj
 
     
@@ -203,17 +270,31 @@ def h(board_state, player):
                 """
     
     for (r, c) in empty_cells:
+<<<<<<< HEAD
         adjacent = get_adjacent_cells(r, c)
+=======
+        adjacent = get_adjacent_cells(r, c, size)
+>>>>>>> 58757c0 (Co-authored-by: Alexxhhh <Alexxhhh@users.noreply.github.com>)
         for (r2, c2) in adjacent:
             for (r3, c3) in adjacent:
                 if (r2, c2) < (r3, c3):
                     a = board[r2][c2]
                     b = board[r3][c3]
+<<<<<<< HEAD
                     if a and b:
                         if a[0] != player and b[0] != player and a[1] + b[1] <= 6:
                             my_caps += 1
                         if a[0] != opponent and b[0] != opponent and a[1] + b[1] <= 6:
                             op_caps += 1
+=======
+                    if a and b and a[0] != player and b[0] != player:
+                        if a[1] + b[1] <= 6:
+                            mycaps+=1
+                    if a and b and a[0] != opponent and b[0] != opponent:
+                        if a[1] + b[1] <= 6:
+                            op_caps+=1
+
+>>>>>>> 58757c0 (Co-authored-by: Alexxhhh <Alexxhhh@users.noreply.github.com>)
     score = (
         10 * (my_pieces - op_pieces) +
          1 * (my_pips   - op_pips) +
